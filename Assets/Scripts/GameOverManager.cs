2e1550bedc287034902a5edfd3b12749d401cd69
akbar zidan <akbarzidan123@gmail.com>
2021-10-10 15:33:34 +0700

    Problem-9

---
+++ Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameOverManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool isGameOver = false;
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGameOver)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+                isGameOver = false;
+            }
+        }
+    }
+    public void Show()
+    {
+        gameObject.SetActive(true);
+        isGameOver = true;
+    }
+}
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TimeManager : MonoBehaviour
+{
+    public Text textTime;
+    public float durtaion;
+    public GameOverManager gameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(durtaion <= 1)
+        {
+            gameOver.Show();
+            Time.timeScale = 0;
+        }
+        textTime.text = GetRemainingTime();   
+    }
+
+    string GetRemainingTime()
+    {
+        int minute = Mathf.FloorToInt(durtaion / 60);
+        int second = Mathf.FloorToInt(durtaion % 60);
+        durtaion -= Time.deltaTime;
+        return string.Format("{0} : {1}", minute.ToString(), second.ToString());
+    }
+}
